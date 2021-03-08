d718a29c27b5c619926ddca107492bce71733e9d
山本大志 <taishi020294@gmail.com>
2021-03-08 22:46:18 +0900

    #24

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   belongs_to :user_classification
 
-  validates :first_name, length: { maximum: 10 }
-  validates :last_name, length: { maximum: 10 }
-  validates :zipcode, length: { is: 7 }, format: { with: /\A[0-9]+\z/ }
-  validates :rprefecture, length: { maximum: 5 }
-  validates :municipality, length: { maximum: 10 }
-  validates :address, length: { maximum: 15 }
-  validates :apartments, length: { maximum: 20 }
-  validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
-  validates :phone_number, length: { maximum: 15 }, format: { with: /\A[0-9]+\z/ }
+  validates :first_name, length: { maximum: 10 }, presence: true
+  validates :last_name, length: { maximum: 10 }, presence: true
+  validates :zipcode, length: { is: 7 }, format: { with: /\A[0-9]+\z/ }, presence: true
+  validates :rprefecture, length: { maximum: 5 }, presence: true
+  validates :municipality, length: { maximum: 10 }, presence: true
+  validates :address, length: { maximum: 15 }, presence: true
+  validates :apartments, length: { maximum: 20 }, presence: true
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }, presence: true
+  validates :phone_number, length: { maximum: 15 }, format: { with: /\A[0-9]+\z/ }, presence: true
 
 end
