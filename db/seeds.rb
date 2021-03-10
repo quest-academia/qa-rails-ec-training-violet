a48f409be5481cc2386e6422b9bdb616231a31e5
山本大志 <taishi020294@gmail.com>
2021-03-10 23:43:18 +0900

    #27 db/seeds.rbの「zipcode,phone_number」の修正、user_classificationの定義づけ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-UserClassification.create!(user_classification_name: "打ち切り漫画家")
-User.create!(first_name: "漫", last_name: "画太郎", zipcode: "1236789",
+user_classification = UserClassification.create!(user_classification_name: "打ち切り漫画家")
+
+User.create!(first_name: "漫", last_name: "画太郎", zipcode: "123-6789",
              prefecture: "珍遊県", municipality: "珍遊記市", address: "漫画2-3-1", apartments: "太郎とゆかいなアパート306号室",
-             email: "mangatarou@example.com", phone_number: "08001345678", password: "sasdfghjkl", user_classification_id: 6)
-User.create!(first_name: "島袋", last_name: "光年", zipcode: "5829740",
+             email: "mangatarou@example.com", phone_number: "080-0134-5678", password: "password", user_classification_id: user_classification.id)
+User.create!(first_name: "島袋", last_name: "光年", zipcode: "582-9740",
               prefecture: "世紀末県", municipality: "リーダー市", address: "ジャンプ7-5", apartments: "たけしマンション704号室",
-              email: "shimabukuro@example.com", phone_number: "08048205881", company_name: "週刊少年ジャンプ", password: "qwertyui", user_classification_id: 6)
+              email: "shimabukuro@example.com", phone_number: "080-4820-5881", company_name: "週刊少年ジャンプ", password: "password", user_classification_id: user_classification.id)
